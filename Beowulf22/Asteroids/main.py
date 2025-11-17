0b69d383b4beef00577b6a1cf6ed9c17e5405317
Beowulf22 <manst-berlin@web.de>
2025-11-17 13:24:55 +0100

    FPS

--- Beowulf22/Asteroids/main.py
+++ Beowulf22/Asteroids/main.py
@@ -5,10 +5,14 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids with pygame version: (%s)" %  pygame.version.ver)
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Game Loop
     while True:
         log_state()
         for event in pygame.event.get():
@@ -16,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = (clock.tick(60)) / 1000
 
 
 if __name__ == "__main__":
